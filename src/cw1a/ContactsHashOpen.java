f325d5d7c1cfcd77c97fdbacbe29bf47916c8cff
oscar <oscar@LAPTOP-5UGD0NB1.Home>
2025-02-24 19:08:55 +0000

    See comments for exact changes and considerations for optimisations going forward. Implemented the remove operation using 'lazy deletion' using a sentinel value with a 'magic name' and zero ID. The avoids the risks posed by setting values to null, by instead preserving the integrity of collision chains, without incurring the computaional/complexity overhead of rebuilding the table either partially or entirely, or the disruption of the hash-value/position dynamic by entry shifting. Further tests will need to be devised and code cleaned up, but core functionality appears currently present.

--- src/cw1a/ContactsHashOpen.java
+++ src/cw1a/ContactsHashOpen.java
@@ -13,7 +13,12 @@ public class ContactsHashOpen implements IContactDB {
     private int tableCapacity;
     private int numEntries;
     private int totalVisited = 0;
-    
+    /**Declare a static final object acting as sentinel for marking deleted entries in the hash table
+     * This allows for null slots (never used), regular Contact objects (i.e. placed in 'active' slots),
+     * and deleted slots (populated by the 'DELETED' sentinel objects) to be distinguished. 
+     * As such, proper probe sequencing is maintained. 
+     */
+    private static final Contact DELETED = new Contact("__DELETED__","0");
 
     private static final double maxLoadFactor = 50.0;
             
@@ -29,13 +34,20 @@ public class ContactsHashOpen implements IContactDB {
     }
 
     /**
-     * Empties the database.
+     * Empties the database, while preserving the markers for 'lasily DELETED' 
+     * Contact objects in the appropriate slots. This faciliates maintenance of
+     * probe sequence structure and, if built upon in future could help to
+     * optimise future insertions.
+     * Further, load factor calculations must account for prior deletions, so
+     * tabs must be kept on deleted entries to accomodate their functionality.
      *
      * @pre true
      */
     public void clearDB() {
         for (int i = 0; i != table.length; i++) {
-            table[i] = null;
+            if (table[i] != DELETED){ //only clear entries that have not been deleted
+                table[i] = null;
+            }
         }
         numEntries = 0;
     }
@@ -59,55 +71,85 @@ public class ContactsHashOpen implements IContactDB {
         return (double) numEntries / (double) table.length * 100.0;
     } 
        
-
+    /**Locates the position in the hash table for a given Contact object denoted
+     * by name. 'Lazy deletion' implemented alongside quadratic probing.
+     * Maintains neccessary chains for collision resolution with quad probing.
+     * Could be augmented in future to minimise clustering by 'remembering',
+     * for possible re-use, the deleted positions.
+     * 
+     * @param name : the name of the Contact object to be located in the HT
+     * @return : the position where that name is found, or the first truly empty
+     * (null) encountered while probing where the name may be inserted to the HT.
+     **/
     private int findPos(String name) {
         assert name != null && !name.trim().equals(""); //trim whitespace and verify name is not null or an empty string
         int startPos = hash(name); //Calculate initial hash position 
         int pos = startPos; //init the position being examined to the initial hash position
         int numVisited = 1; //init counter for num positions checked
         //Debugger print statements
+        
         System.out.println("\nStarting search for: " + name);
         System.out.println("Initial hash position: " + startPos);
-        //check whether the initial position is either empy (null - maybe change later...), or contains target
+        
+
+        
+        //check whether the initial position is either empy (null - maybe change
+        //later...), or contains target.
         //If so for either, no probing needed so can skip to return
-        if (table[pos] == null || name.equals(table[pos].getName())) {
-            System.out.println("Search ended at position " + pos);
-            if (table[pos]!= null){
-                System.out.println("Found: " + table[pos].getName());
-            } else {
-                System.out.println("Position is empty");
-            }
-            totalVisited+=numVisited; //update count prior to returning
+        if (table[pos] == null){
+            System.out.println("Search ended at position " + pos + "(empty)");
+            totalVisited += numVisited;
             return pos;
+            
+        //or, if the target was found at the initial position, and has not been
+        //deleted, no need for probing either, so return that position.     
+        } else if (table[pos]!= DELETED && name.equals(table[pos].getName())) {
+                System.out.println("Search ended at position " + pos + "(found: " + table[pos].getName() + ")");
+                totalVisited += numVisited;
+                return pos;                
         }
-        //If initial position was not empty or target, commence quad probing
+
+        //commence quad probing if the initial position is not appropriate.
         for (int i=1; numVisited < table.length; i++){
             //keep track of bucket trail for analysis
            System.out.println("Visiting bucket " + pos + ": " + table[pos] );
            
-           int step = i * i;//the "i^2" step of the formula
+           /**apply the quadratic formula h(k,i) = (h'(k) + i^2) mod m , where:
+            * h'(k) is the initit hash value for key, denoted by startPos
+            * i denotes the number representing the current probe attempt count
+            * i^2 represents the step size, facilitating quadratic expansion 
+            * m is the table capacity (set elsewhere to prime), i.e. table.length
+            * the result being the next pos in the table to be checked.
+            */
+           int step = i * i;
            //Calculate the new position using quad formula
-           //Double up the mod to ensure positive result even if overflow leads the first statement to yield a negative
+           //Double up the mod to ensure positive result even if overflow leads
+           //the first statement to yield a negative
            pos = ((startPos + step) % table.length + table.length) % table.length;
            numVisited++;//inc counter 
            //Debugger print stmnt
            System.out.println("Next step size: " + step + ",moving to position: " + pos);
-           //break out of probing loop if null or target found (look at revising without the dreaded 'break')
-           if (table[pos] == null || name.equals(table[pos].getName())){
+           
+           if (table[pos] == null){
+              break;
+           //or, if the target has been found...    
+           }else if (table[pos] != DELETED && name.equals(table[pos].getName())){
                break;
-            }
+              
+           }
+           
         }
         //output trail and result for analysis  
-        System.out.print("Search ended at position " +pos);
-        if (table[pos] != null){
+        //System.out.print("Search ended at position " +pos);
+        if (table[pos] != null && table[pos] != DELETED){
             System.out.println("Found " + table[pos].getName());
         } else {
-            System.out.println("Position is empty");
+            System.out.println("Position is empty or has been deleted");
         }
         System.out.println("number of  buckets visited = " + numVisited);
         totalVisited += numVisited;
       
-        assert table[pos] == null || name.equals(table[pos].getName());
+        //assert table[pos] == null || name.equals(table[pos].getName());
         return pos;
     }
 
@@ -116,12 +158,13 @@ public class ContactsHashOpen implements IContactDB {
      *
      * @pre name not null or empty string
      * @param name the Contact name (key) to locate
-     * @return true iff the name exists as a key in the database
+     * @return true iff the name exists as a key in the database to a non-DELETED
+     * slot
      */
     public boolean containsName(String name) {
         assert name != null && !name.equals("");
-        int pos = findPos(name);
-        return get(name) != null;
+        Contact contact = get(name);
+        return contact != null && contact != DELETED;
     }
 
     /**
@@ -135,14 +178,12 @@ public class ContactsHashOpen implements IContactDB {
     @Override
     public Contact get(String name) {
         assert name != null && !name.trim().equals("");
-        Contact result;
-        int pos = findPos(name);       
-        if (table[pos] == null) {
-            result = null; // not found
-        } else {
-            result = table[pos];
-        }
-        return result;
+        int pos = findPos(name);
+        //null is returned for both empty and DELETED entries
+        if (table[pos] == null || table[pos] == DELETED) {
+            return null; // not found
+        } 
+        return  table[pos];       
     }
 
     /**
@@ -199,7 +240,7 @@ public class ContactsHashOpen implements IContactDB {
 
     /**
      * Removes and returns a contact from the database, with the key the
-     * supplied name.
+     * supplied name, using lazy deletion.
      *
      * @param name The name (key) to remove.
      * @pre name not null or empty string
@@ -209,9 +250,16 @@ public class ContactsHashOpen implements IContactDB {
     public Contact remove(String name) {
         assert name != null && !name.trim().equals("");
         int pos = findPos(name);
-        System.out.println("remove not yet implemented");
+        Contact removedContact = null; // initialise
         
-        return null;
+        //Only 'DELETE' if a non-null, non-DELETED, matching entry
+        if (table[pos] != null && table[pos] != DELETED && name.equals(table[pos].getName())){
+            removedContact = table[pos];
+            table[pos] = DELETED; //rather than using null, mark as DELETED
+            numEntries--; //decrement size, replicating the removal of an entry
+        }
+        
+        return removedContact;
     }
 
     /**
@@ -267,7 +315,11 @@ public class ContactsHashOpen implements IContactDB {
             }
         }
     }
-
+    /**
+     * When loadFactor > the assigned max, resizes the hash table, carrying over 
+     * only non-DELETED entries, clearing deletion markers and rehashing all the
+     * new entries into the new, expanded table. 
+     */
     private void resizeTable() { // mkae a new table of greater capacity and rehashes old values into it
         System.out.println("RESIZING");
         Contact[] oldTable = table; // copy the reference
@@ -276,12 +328,12 @@ public class ContactsHashOpen implements IContactDB {
         
         tableCapacity = PRIME_SIZES[primeIndex];
         System.out.println("resizing to " + tableCapacity);
-        table = new Contact[tableCapacity]; // make a new tyable
+        table = new Contact[tableCapacity]; // make a new table
         
         clearDB();
         numEntries = 0;
         for (int i = 0; i != oldTableCapacity; i++) {
-            if (oldTable[i] != null) { // dleted vakues not hashed across
+            if (oldTable[i] != null && oldTable[i] != DELETED) { // dleted vakues not hashed across
                 putWithoutResizing(oldTable[i]);
             }
         }
